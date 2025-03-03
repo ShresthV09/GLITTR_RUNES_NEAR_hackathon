236a981f2913f1ba3b4677d9a2cd21ce7540aeb0
Shresth Verma <shresthv1@gmail.com>
2025-03-04 04:58:02 +0530

    finalV

--- components/client/ClientDashboard.tsx
+++ components/client/ClientDashboard.tsx
@@ -18,7 +18,7 @@ const ClientDashboard: React.FC = () => {
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
       <div className="lg:col-span-2 space-y-6">
         <JobPostingCard />
-        <ContractCreationCard />
+       
         <TrustLockProcessCard />
         <ActiveEscrowsCard jobs={jobs} userType="client" />
       </div>
